c340eeba4f11a70e5afd5ac317c44f7316bcbf09
SoldzToys <43523244+SoldzToys@users.noreply.github.com>
2018-10-03 10:51:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ client.on('message', async (message) => {
 	.setColor("#2387c3")
 	.addField("Ping Speed", 'Pong! Your ping is `' + `${Date.now() - message.createdTimestamp}` + ' ms`')
 	  message.channel.send(pingembed);
-  
   }
-	  
+	   });
+	
 client.on('guildMemberAdd', (member) => {
 	
   let guild = member.guild;
@@ -36,6 +36,6 @@ client.on('guildMemberAdd', (member) => {
   logs.send(gembed);
 	      });
 	
-            });
+           
 client.login(process.env.BOT_TOKEN);
 
