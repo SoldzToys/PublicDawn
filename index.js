b799b5be92fad18ca33f9217afbf37058da9eaa2
SoldzToys <43523244+SoldzToys@users.noreply.github.com>
2018-10-03 01:39:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ if(guild.systemChannel){
 	guild.systemChannel.send(new Discord.RichEmbed() 
 	.setTitle("PONG!") 	 
 	.setColor("#2387c3")
-	message.channel.send('Pong! Your ping is `' + `${Date.now() - message.createdTimestamp}` + ' ms`);
+	message.channel.send('Pong! Your ping is `' + `${Date.now() - message.createdTimestamp}` + ' ms`))
 
         }
         
