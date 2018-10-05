8af59754fd6eca46b1f53e6428d2fd0b13c9e7ae
SoldzToys <43523244+SoldzToys@users.noreply.github.com>
2018-10-05 14:55:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -100,39 +100,6 @@ client.on('message', async (message, args) => {
 	
 if (message.content === `${prefix}ban`) {
 	
-	if(!message.member.hasPermission("MANAGE_MESSAGES"))
-    return message.reply("You don't have the permissions to manage messasges, you will not be able to do this command.");
-    if(args[0] == "help"){
-      message.reply("Usage: ~ban <user> <reason>");
-      return;
-    }
+let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!bUser) return message.channel.send("You haven't @selected a user to ban.");
 	
-let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0])); 
-	if (!bUser) return message.channel.send(`I couldn't the find user.`);
-	let  bReason = args.slice(1).join(" ") || "None";
-	if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(`You don't have the permissions to manage messasges, you will not be able to do this command.`);
-	if (bUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send(`This user can't be banned! They are either the same rank or higher then you.`);
-
-
-
-    let banEmbed = new Discord.RichEmbed()
-    .setDescription("Ban Report")
-    .setColor("#c2dbea")
-    .addField("Banned User", `${bUser} with ID ${bUser.id}`)
-    .addField("Banned By", `<@${message.author.id}> with ID ${message.author.id}`)
-    .addField("Banned In", message.channel)
-    .addField("Time", message.createdAt)
-    .addField("Reason", bReason)
-    .setTimestamp(new Date());
-  
-    let banChannel = message.guild.channels.find(c => c.name === 'logging');
-    message.guild.member(bUser).ban(bReason);
-    banChannel.send(banEmbed);
-	
-    return message.channel.send(`${bUser} has been launched back out into space! BANNED!!!`);
-}
-});
-	
-           
-client.login(process.env.BOT_TOKEN);
-
