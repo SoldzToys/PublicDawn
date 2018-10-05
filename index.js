f4b377dc7304523244b69785fabffd08c030122d
SoldzToys <43523244+SoldzToys@users.noreply.github.com>
2018-10-05 19:39:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -107,6 +107,7 @@ return message.reply("You don't have the permissions to manage messasges, you wi
 		let args = message.content.slice(1).split(" ");
 	let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!bUser) return message.channel.send("You haven't selected/mentioned a user to ban.");
+	    let bReason = args.slice(1).join(" ") || "None";
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have the permissions to manage messasges, you will not be able to do this command.");
     if(bUser.hasPermission("MANAGEmessage.content._MESSAGES")) return message.channel.send("This user can't be banned! They are either the same rank or higher then you.");
   
