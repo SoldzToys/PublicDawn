b6e1821745ad01853bf2a61149a3b7a2f7987733
SoldzToys <43523244+SoldzToys@users.noreply.github.com>
2018-10-05 19:36:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -106,8 +106,7 @@ return message.reply("You don't have the permissions to manage messasges, you wi
 	
 		let args = message.content.slice(1).split(" ");
 	let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!bUser) return message.channel.send("You haven't @selected/mentioned a user to ban.");
-	let bReason = args.slice(1).join(" ") || "None";
+    if(!bUser) return message.channel.send("You haven't selected/mentioned a user to ban.");
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have the permissions to manage messasges, you will not be able to do this command.");
     if(bUser.hasPermission("MANAGEmessage.content._MESSAGES")) return message.channel.send("This user can't be banned! They are either the same rank or higher then you.");
   
@@ -121,7 +120,7 @@ return message.reply("You don't have the permissions to manage messasges, you wi
     .addField("Reason", bReason)
     .setTimestamp(new Date());
   
-    let banChannel = message.guild.channels.find(c => c.name === 'logs');
+    let banChannel = message.guild.channels.find(c => c.name === 'logging');
     if(!banChannel) return message.channel.send("I can't find logging channel.");
   
     message.guild.member(bUser).ban(bReason);
