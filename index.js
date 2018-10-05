4fb222572647527a19879c2e7f21408a4bb7948c
SoldzToys <43523244+SoldzToys@users.noreply.github.com>
2018-10-05 14:37:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -102,11 +102,10 @@ if (message.content === `${prefix}ban`) {
 	
 let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0])); 
 	if (!bUser) return message.channel.send(`I couldn't the find user.`);
-	let  bReason = args.slice(1).join(" ") || "None";
 	if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(`You don't have the permissions to manage messasges, you will not be able to do this command.`);
 	if (bUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send(`This user can't be banned! They are either the same rank or higher then you.`);
 
-	
+let  bReason = args.slice(1).join(" ") || "None";
 
     let banEmbed = new Discord.RichEmbed()
     .setDescription("Ban Report")
