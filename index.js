edbca9a8b24b1167fba9a31e8acbd7ccdce7591c
SoldzToys <43523244+SoldzToys@users.noreply.github.com>
2018-10-06 19:52:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -187,7 +187,6 @@ let rMember = message.mentions.members.first() || message.guild.members.get(args
   if (!gRole) return message.channel.send("I couldn't find them.");
   if(rMember.roles.has(gRole.id));
   return message.channel.send("They already have this role.");
-  } else {
   await(rMember.addRole(gRole.id));
   }
   try{
