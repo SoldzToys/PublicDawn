252f383a6da983473ff52dce9aed995ab3f0ff0e
SoldzToys <43523244+SoldzToys@users.noreply.github.com>
2018-10-06 14:00:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -99,14 +99,14 @@ client.on("channelDelete", async (channel) => {
 client.on('message', async (message) => {
 
 	
-if (!message.content.startsWith(`${prefix}ban`)) {
+if (!message.content.startsWith(`${prefix}ban`)) 
 
-let args = message.content.slice(1).split(" ");	
+
 if(!message.member.hasPermission("MANAGE_MESSAGES"))
 return message.reply("You don't have the permissions to manage messages, you will not be able to do this command.");
 
 	
-		
+	let args = message.content.slice(1).split(" ");		
 	let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!bUser) return message.channel.send("You haven't selected/mentioned a user to ban.");
 	    let bReason = args.slice(1).join(" ") || "None";
