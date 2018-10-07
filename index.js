ebdf16038b9bb07892d3ac764e655715250ad658
SoldzToys <43523244+SoldzToys@users.noreply.github.com>
2018-10-06 23:27:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -171,6 +171,33 @@ return message.channel.send(`${kUser} has been kicked from the server!`)
 }
 	
 });
+client.on('message', async (message) => {
+
+ let member = message.mentions.users.first() || message.author
+            let player = message.mentions.members.first() || message.member
+            let user = message.mentions.users.first();
+            let iicon = player.user.displayAvatarURL;
+            let roles = message.member.roles.map(role => role).join(" ")
+        if(!user) return message.channel.send("I couldn't the find user.");
+            let userEmbed = new Discord.RichEmbed()
+            .setAuthor(`${user.username}'s Info`, user.displayAvatarURL)
+            .setThumbnail(user.displayAvatarURL)
+            .setColor('#FF8C00')
+            .addField('ID', user.id, true)
+            .addField('Current Tag', user.tag, true)
+            .addField('Nickname', `${player.displayName}`, true) 
+            .addField('Highest Member Role', `${player.highestRole.name}`, true)
+            .addField('Roles', `${roles}`)
+            .addField('Game/Playing', `${(user.presence.game && user.presence.game && user.presence.game.name) || 'None'}`, true)
+            .addField('Status', user.presence.status, true)
+            .addField('Bot', user.bot, true)
+            .addField('Joined At:', `${player.joinedAt}`)
+            .addField('Created At:', `${player.user.createdAt}`)
+            .setThumbnail(iicon)
+            .setTimestamp(new Date());
+            message.channel.send(userEmbed)
+            }        
+});
 
 client.on('message', async (message) => {
 	
