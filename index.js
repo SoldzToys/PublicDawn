9dc1d3e41d07ef8765120b3278b58f4842216a65
SoldzToys <43523244+SoldzToys@users.noreply.github.com>
2018-10-07 16:18:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -280,7 +280,6 @@ await rMember.addRole(gRole.id)
     await rMember.send(`You've been given the ${gRole.name} role.`)
  }catch(e){
 	 message.channel.send(`You've been given the <@${rMember.id}> ${gRole.name} role. Those DMs aren't opened though.`)
- }
 });
 	
 client.login(process.env.BOT_TOKEN);
