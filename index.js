acf8e28522cd7a922de762b9cbfdbbf319105708
SoldzToys <43523244+SoldzToys@users.noreply.github.com>
2018-10-07 01:40:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ client.on('message', async (message) => {
 	   let user = message.mentions.users.first();
 if(!user) return message.channel.send("You haven't selected/mentioned a user whose avatar you want to see.");
     let avatarEmbed = new Discord.RichEmbed()
-    .setAuthor(`${user.username}`)
-    .setImage(user.displayAvatarURL)
+    .setAuthor(`${user.username}'s Profile Picture`)
+    .setImage(user.user.displayAvatarURL)
     .setColor("#ea9b67")
     .setTimestamp(new Date());
     message.channel.send(avatarEmbed);
