c3de66375d509cde6fd98a9e859f1245ef3e49e5
SoldzToys <43523244+SoldzToys@users.noreply.github.com>
2018-10-07 05:24:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -277,9 +277,9 @@ let rMember = message.mentions.members.first() || message.guild.members.get(args
   return message.channel.send("They already have this role.");
 await rMember.addRole(gRole.id)
   }
-  try{
+  try{}
     await rMember.send(`You've been given the ${gRole.name} role.`)
-  }
+  
 });
 	
 client.login(process.env.BOT_TOKEN);
